5d3f0d8088a6a490791b6bdd6eef8baa24f60a2f
Rahbar Ali <112726734+Rahbar-Ali@users.noreply.github.com>
2025-03-01 23:46:15 +0500

    update

--- src/components/CoffeeForm.jsx
+++ src/components/CoffeeForm.jsx
@@ -1,10 +1,127 @@
+import { useState } from "react";
+import { coffeeOptions } from "../utils";
+
 const CoffeeForm = () => {
+  const [selectedCoffee, setSelectedCoffe] = useState(false);
+  const [showCoffeeOther, setShowCoffeeOther] = useState(false);
+  const [coffeeCost, setCoffeeCost] = useState(0);
+  const [hour, setHour] = useState(0);
+  const [min, setMin] = useState(0);
+
+  function handleSubmitForm() {
+    console.log(selectedCoffee, coffeeCost, hour, min);
+  }
+
   return (
     <>
       <div className="section-header">
         <i className="fa-solid fa-pencil" />
         <h2>Start Tracking Today</h2>
       </div>
+      <h4>Select coffee type</h4>
+      <div className="coffee-grid">
+        {coffeeOptions.slice(0, 5).map((option, optionIndex) => {
+          return (
+            <button
+              onClick={() => {
+                setSelectedCoffe(option.name);
+                setShowCoffeeOther(false);
+              }}
+              className={
+                "button-card " +
+                (option.name === selectedCoffee ? "coffee-button-selected" : "")
+              }
+              key={optionIndex}
+            >
+              <h4>{option.name}</h4>
+              <p>{option.caffeine}mg</p>
+            </button>
+          );
+        })}
+        <button
+          onClick={() => {
+            setShowCoffeeOther(true);
+            setSelectedCoffe(null);
+          }}
+          className={
+            "button-card " + (showCoffeeOther ? "coffee-button-selected" : "")
+          }
+        >
+          <h4>Other</h4>
+          <p>n/a</p>
+        </button>
+      </div>
+      {showCoffeeOther && (
+        <select
+          onChange={(e) => {
+            setSelectedCoffe(e.target.value);
+          }}
+          id="coffee-list"
+          className="coffee-list"
+        >
+          <option value={null}>Select type</option>
+          {coffeeOptions.map((option, optionIndex) => {
+            return (
+              <option value={option.name} key={optionIndex}>
+                {option.name} ({option.caffeine}mg)
+              </option>
+            );
+          })}
+        </select>
+      )}
+      <h4>Add the cost ($)</h4>
+      <input
+        value={coffeeCost}
+        onChange={(e) => {
+          setCoffeeCost(e.target.value);
+        }}
+        className="w-full"
+        type="number"
+        placeholder="4.5"
+      />
+      <h4>Time since consumption</h4>
+      <div className="time-entry">
+        <div>
+          <h4>Hours</h4>
+          <select
+            onChange={(e) => {
+              setHour(e.target.value);
+            }}
+            id="hours-select"
+          >
+            {[
+              0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+              19, 20, 21, 22, 23,
+            ].map((hour, hourIndex) => {
+              return (
+                <option key={hourIndex} value={hour}>
+                  {hour}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <div>
+          <h4>Hours</h4>
+          <select
+            onChange={(e) => {
+              setMin(e.target.value);
+            }}
+            id="hours-select"
+          >
+            {[0, 5, 10, 15, 30, 45].map((min, minIndex) => {
+              return (
+                <option key={minIndex} value={min}>
+                  {min}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+      </div>
+      <button onClick={handleSubmitForm}>
+        <p>Add Entry</p>
+      </button>
     </>
   );
 };
--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,5 +1,43 @@
+import {
+  calculateCurrentCaffeineLevel,
+  coffeeConsumptionHistory,
+  getCaffeineAmount,
+  timeSinceConsumption,
+} from "../utils";
+
 const History = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="section-header">
+        <i className="fa-solid fa-timeline " />
+        <h2>Header</h2>
+      </div>
+      <p>
+        <i>Hover for more information!</i>
+      </p>
+      <div className="coffee-history">
+        {Object.keys(coffeeConsumptionHistory)
+          .sort((a, b) => b - a)
+          .map((utcTime, coffeeIndx) => {
+            const coffee = coffeeConsumptionHistory[utcTime];
+            const timeSinceConsume = timeSinceConsumption(utcTime);
+            const orginalAmount = getCaffeineAmount(coffee.name);
+            const remainingAmount = calculateCurrentCaffeineLevel({
+              [utcTime]: coffee,
+            });
+
+            const summary = `${coffee.name} | ${timeSinceConsume} | ${orginalAmount}mg / ${remainingAmount}`;
+
+            return (
+              <div title={summary} key={coffeeIndx}>
+                {" "}
+                <i className="fa-solid fa-mug-hot" />
+              </div>
+            );
+          })}
+      </div>
+    </>
+  );
 };
 
 export default History;
